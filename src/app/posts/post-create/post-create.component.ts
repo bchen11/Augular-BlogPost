2572c37fe74e787df1e11f88726e373b94ee1a23
binlinchen <42919196+bchen11@users.noreply.github.com>
2021-12-15 16:17:55 -0500

    add post component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,14 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  newPost = '';
+  enteredValue = '';
+  newPost = 'Hi';
 
 
 
   onAddPost(){
-    this.newPost = 'The user\'s post';
+    this.newPost = this.enteredValue;
   }
 }
