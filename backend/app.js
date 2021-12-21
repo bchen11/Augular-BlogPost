d1f947d2c8e6d328dfa723aba28f5a3456ff9ed7
binlinchen <42919196+bchen11@users.noreply.github.com>
2021-12-20 19:58:33 -0500

    add delete api for post

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://bchen11:1331@cluster0.ra34u.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(() =>{
+    console.log('Connected to DB');
+})
+.catch(() => {
+    console.log('Connection failed');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,31 +25,33 @@ app.use((req,res,next) => {
 })
 
 app.post('/api/posts', (req,res,next) => {
-    const post = req.body;
-    console.log(post);
-    res.status(201).json({
-        message: "Post Added"
-    }
-    );
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    })
+    post.save().then(createdPost =>{
+         res.status(201).json({
+        message: "Post Added",
+        postId: createdPost._id
+    });
+   });
 });
 
 app.get('/api/posts',(req,res,next) =>{
-    const posts = [
-        {
-            id: 'fsaf2132312',
-            title: 'First server-side post',
-            content: 'this is coming from the server'
-        },
-        {
-            id: 'sfsf12412555',
-            title: 'Second server-side post',
-            content: 'this is coming from the server!'
-        }
-    ];
+    Post.find()
+    .then(documents =>{
+        res.status(200).json({
+            message: 'Posts fetched succesfully',
+            posts: documents
+        })
+    });
+});
 
-    res.status(200).json({
-        message: 'Posts fetched succesfully',
-        posts: posts
+
+app.delete("/api/posts/:id", (req,res,next) => {
+    Post.deleteOne({_id: req.params.id}).then(result => {
+        console.log(result);
+        res.status(200).json({message: "Post Deleted"});
     })
 });
 
