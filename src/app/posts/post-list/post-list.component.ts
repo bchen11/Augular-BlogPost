c2cfb07791e9705b3ca3b1e7fb57e99fd9f04812
binlinchen <42919196+bchen11@users.noreply.github.com>
2022-01-02 14:57:23 -0500

    add pagination feature

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
+import { PageEvent } from "@angular/material/paginator";
 import { Subscription } from "rxjs";
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
@@ -18,6 +19,10 @@ export class PostListComponent implements OnInit, OnDestroy {
 
     posts: Post[] = [];
     isLoading = false;
+    totalPosts = 0;
+    postPerPage = 2;
+    currentPage = 1
+    pageSizeOptions = [1,2,5,10];
     private postsSub: Subscription
 
 
@@ -25,16 +30,28 @@ export class PostListComponent implements OnInit, OnDestroy {
    constructor(public postsService : PostsService) {}
    ngOnInit(){
        this.isLoading = true;
-       this.postsService.getPosts();
+       this.postsService.getPosts(this.postPerPage, this.currentPage);
        this.postsSub = this.postsService.getPostUpdateListener()
-       .subscribe((posts: Post[]) =>{
-           this.isLoading = false;
-            this.posts = posts;
+       .subscribe((postData: {posts : Post[], postCount: number }) =>{
+            this.isLoading = false;
+            this.totalPosts = postData.postCount;
+            this.posts = postData.posts;
        });
 }
 
+
+    onChangedPage(pageData : PageEvent){
+        this.isLoading = true;
+        this.currentPage = pageData.pageIndex + 1;
+        this.postPerPage = pageData.pageSize;
+        this.postsService.getPosts(this.postPerPage, this.currentPage);
+    }
+
     onDelete(postId: string) {
-        this.postsService.deletePost(postId);
+        this.isLoading = true;
+        this.postsService.deletePost(postId).subscribe(() =>{
+            this.postsService.getPosts(this.postPerPage, this.currentPage);
+        });
     }
 
 
